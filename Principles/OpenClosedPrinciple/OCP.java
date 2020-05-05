27fcb2b6053a18ff6a520e2e8278cebc034bf9e8
PhillByrd2018 <Phill.e.byrd@gmail.com>
2020-05-05 16:48:42 -0700

    Day 2 principles

--- Principles/OpenClosedPrinciple/OCP.java
+++ Principles/OpenClosedPrinciple/OCP.java
@@ -1,4 +1,4 @@
-package OpenClosedPrinciple;
+package Principles.OpenClosedPrinciple;
 
 import java.util.List;
 import java.util.stream.Stream;
--- Principles/SingleResponsibilityPrinciple/SRP.java
+++ Principles/SingleResponsibilityPrinciple/SRP.java
@@ -1,4 +1,4 @@
-package SingleResponsibilityPrinciple;
+package Principles.SingleResponsibilityPrinciple;
 
 import java.io.File;
 import java.io.PrintStream;
